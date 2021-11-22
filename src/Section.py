b7e57c352911b949c0c49cc44cff5ca87e183fbc
AlixSMB <90306694+AlixSMB@users.noreply.github.com>
2021-11-22 14:42:54 +0100

    Add files via upload

--- src/Section.py
+++ src/Section.py
@@ -52,12 +52,26 @@ class Section:
 
     # Defines the vertices and faces 
     def generate(self):
-        self.vertices = [ 
-                # Définir ici les sommets
-                ]
-        self.faces = [
-                # définir ici les faces
-                ]   
+		self.vertices = [ 
+			 # devant
+			[0, 0, 0],
+			[0, 0, self.parameters['height']], 
+			[self.parameters['width'], 0, self.parameters['height']],
+			[self.parameters['width'], 0, 0],
+			# derriere
+			[0, self.parameters['thickness'], 0], 
+			[self.parameters['width'], self.parameters['thickness'], 0], 
+			[self.parameters['width'], self.parameters['thickness'], self.parameters['height']],
+			[0, self.parameters['thickness'], self.parameters['height']],	 
+		]
+		self.faces = [ # sens anti-horaire
+			[0, 3, 2, 1], # devant
+			[4, 7, 6, 5], # derriere
+			[0, 1, 7, 4], # gauche
+			[2, 3, 5, 6], # droite
+			[1, 2, 6, 7], # haut
+			[3, 0, 4, 5]  # bas
+		]    
 
     # Checks if the opening can be created for the object x
     def canCreateOpening(self, x):
@@ -77,5 +91,11 @@ class Section:
     # Draws the faces
     def draw(self):
         # A compléter en remplaçant pass par votre code
-        pass
+		gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_FILL) # on trace les faces : GL_FILL
+		for face in self.faces:
+			gl.glBegin(gl.GL_QUADS) 
+			gl.glColor3fv([0.5]*3)
+			for pointInd in face: 
+				gl.glVertex3fv(self.vertices[pointInd])
+			gl.glEnd()
   
